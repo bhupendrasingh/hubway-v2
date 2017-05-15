fac7ddd3f592aade0215cf1022df96621892975b
bhupendrasingh <bhupendrasingh@ccs.neu.edu>
2017-05-14 20:55:28 -0400

    marker broken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './data.service';
-import {Station} from './station';
+import { Station } from './station';
 declare var google: any;
+var myMap: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +10,7 @@ declare var google: any;
   providers: [DataService]
 })
 export class AppComponent implements OnInit {
-  stations:Station[];
+  stations: Station[];
   errorMessage: string;
   mode = 'Observable';
 
@@ -17,16 +18,50 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    myMap = new google.maps.Map(document.getElementById('map'), {
       center: { lat: 42.3601, lng: -71.0589 },
       scrollwheel: false,
-      zoom: 10
+      zoom: 16
     });
+    this.getStationList();
+    // this.createMapMarkers();
   }
   getStationList() {
     this._dataservice.loadStations()
       .subscribe(stations => {
         this.stations = stations;
+       setTimeout(function(){
+
+       },10000);
+        this.createMapMarkers(this.stations);
       });
+  }
+
+  createMapMarkers(stationList) {
+    var prev_infowindow = false;
+    let stationsArray = stationList['data']['stations'];
+    for (var _i = 0; _i < stationsArray.length; _i++) {
+      // console.log(myMap);
+      // console.log( stationsArray[_i]['lat']);
+      var marker = new google.maps.Marker({
+        postion: {
+          lat: stationsArray[_i]['lat'],
+          lon: stationsArray[_i]['lon'],
+        },
+        // map: myMap,
+        title: stationsArray[_i]['name'],
+        visible: true
+      });
+      marker.setMap(myMap);
+      // console.log(marker);
+      marker.addListener('click', function () {
+        if (prev_infowindow) {
+          // prev_infowindow.close();
+          alert("hello");
+        }
+        // generate_status(id, map, marker)
+        //infowindow.open(map,marker);
+      });
+    }
   }
 }
